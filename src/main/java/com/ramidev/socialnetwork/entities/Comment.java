5b841c7d54229c27c0343f798a303adbd74c0e3a
siprem10 <siprem10@gmail.com>
2022-12-30 15:23:28 -0300

    feature(post&comment Entities & relationShip)

--- src/main/java/com/ramidev/socialnetwork/entities/Comment.java
+++ src/main/java/com/ramidev/socialnetwork/entities/Comment.java
@@ -1,11 +1,43 @@
 package com.ramidev.socialnetwork.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comment")
+@Getter @Setter @ToString
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotNull
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @CreationTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
 }
--- src/main/java/com/ramidev/socialnetwork/entities/Post.java
+++ src/main/java/com/ramidev/socialnetwork/entities/Post.java
@@ -1,9 +1,23 @@
 package com.ramidev.socialnetwork.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.security.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "post")
+@Getter @Setter @ToString
 public class Post {
 
     @Id
@@ -15,4 +29,28 @@ public class Post {
 
     @Column(name = "image_url")
     private String imageUrl;
+
+    @NotNull
+    @CreationTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+
+    @NotNull
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+        comment.setPost(this);
+    }
+    public void removeComment(Comment comment) {
+        comments.remove(comment);
+        comment.setPost(null);
+    }
 }
