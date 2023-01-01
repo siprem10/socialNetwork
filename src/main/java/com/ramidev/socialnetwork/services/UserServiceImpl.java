b12cc96670273ec950b17f9144fa1d3cc42bcaac
siprem10 <siprem10@gmail.com>
2023-01-01 15:00:46 -0300

    refactor(delete unused import)

--- src/main/java/com/ramidev/socialnetwork/services/UserServiceImpl.java
+++ src/main/java/com/ramidev/socialnetwork/services/UserServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
