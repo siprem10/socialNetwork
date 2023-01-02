d1a734f470547d8818f626bb5c1a97bd443e67c2
siprem10 <siprem10@gmail.com>
2023-01-02 02:54:58 -0300

    feature(added beans)

--- src/main/java/com/ramidev/socialnetwork/SocialnetworkApplication.java
+++ src/main/java/com/ramidev/socialnetwork/SocialnetworkApplication.java
@@ -1,7 +1,11 @@
 package com.ramidev.socialnetwork;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SocialnetworkApplication {
@@ -10,4 +14,15 @@ public class SocialnetworkApplication {
 		SpringApplication.run(SocialnetworkApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return objectMapper;
+	}
 }
