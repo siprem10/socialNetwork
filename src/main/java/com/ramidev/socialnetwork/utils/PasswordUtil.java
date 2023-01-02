06849cd2f4a4558ac9fec9ccbee1d63252f48269
siprem10 <siprem10@gmail.com>
2023-01-02 02:54:46 -0300

    refactor(PwdUtil)

--- src/main/java/com/ramidev/socialnetwork/utils/PasswordUtil.java
+++ src/main/java/com/ramidev/socialnetwork/utils/PasswordUtil.java
@@ -2,7 +2,7 @@ package com.ramidev.socialnetwork.utils;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class Password {
+public class PasswordUtil {
 
     public static String pwdEncoder(String password) {
         return new BCryptPasswordEncoder().encode(password);
