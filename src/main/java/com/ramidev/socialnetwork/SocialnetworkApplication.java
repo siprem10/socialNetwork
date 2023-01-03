820779e4465bcec26367ce78e3fed9c4faddd396
siprem10 <siprem10@gmail.com>
2023-01-03 03:38:07 -0300

    fix(Mapper bean)

--- src/main/java/com/ramidev/socialnetwork/SocialnetworkApplication.java
+++ src/main/java/com/ramidev/socialnetwork/SocialnetworkApplication.java
@@ -2,9 +2,12 @@ package com.ramidev.socialnetwork;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -22,6 +25,8 @@ public class SocialnetworkApplication {
 	@Bean
 	public ObjectMapper objectMapper() {
 		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		return objectMapper;
 	}
