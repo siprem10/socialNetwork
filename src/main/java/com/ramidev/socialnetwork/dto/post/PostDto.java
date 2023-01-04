b6fd30ef55444844dc402e5a0428b64e5cc0dd93
siprem10 <siprem10@gmail.com>
2023-01-04 16:35:05 -0300

    refactor(timestamps)

--- src/main/java/com/ramidev/socialnetwork/dto/post/PostDto.java
+++ src/main/java/com/ramidev/socialnetwork/dto/post/PostDto.java
@@ -1,5 +1,6 @@
 package com.ramidev.socialnetwork.dto.post;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ramidev.socialnetwork.entities.Comment;
 import lombok.Data;
 
@@ -12,8 +13,11 @@ public class PostDto {
     private Long id;
     private String description;
     private String imageUrl;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime createdDate;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime updatedDate;
     private ProfileFromPostDto profile;
     private Set<Comment> comments = new HashSet<>();
+
 }
